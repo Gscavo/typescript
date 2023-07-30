154ec8b27b5af6be62175efc7d5104299e8f8490
Gscavo <gsscavo@gmail.com>
2023-07-30 10:31:37 -0300

    intro

---
+++ 01intro/variablesMe.ts
@@ -0,0 +1,13 @@
+// string
+let greetings: string = 'Hello Guilherme!'
+
+// number
+let userId: number = 334455.3;
+// Doesn't have to put this every time, ts can infer that its a number
+
+// boolean
+let isLoggedIn: boolean = false;
+
+console.log(greetings);
+
+export {}
